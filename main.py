574ecb59b748c24ccdec1d1a40d41830bd79d93b
erezerez1 <Klum1234@hotmail.com>
2024-10-14 17:01:07 +0300

    Update main.py
    added electric car leaf

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ my_new_car = Car('audi', 'a4', 2024)
   print(my_new_car.get_descriptive_name())
 my_new_car.odometer_reading = 23
 my_new_car.read_odometer()
+my_leaf = ElectricCar('nissan', 'leaf', 2024)
+  print(my_leaf.get_descriptive_name())
+my_leaf.battery.describe_battery()
