a0c80dbe0caeed3b14863cc4934a267136db472e
Erez <klum@hotmail.com>
2024-10-15 10:54:27 +0300

    finally works

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from car import car
-from ecar import ElectricCar
+from car import Car,ElectricCar
 
 my_new_car = Car('audi', 'a4', 2024)
-  print(my_new_car.get_descriptive_name())
+print(my_new_car.get_descriptive_name())
 my_new_car.odometer_reading = 23
 my_new_car.read_odometer()
 my_leaf = ElectricCar('nissan', 'leaf', 2024)
-  print(my_leaf.get_descriptive_name())
+print(my_leaf.get_descriptive_name())
 my_leaf.battery.describe_battery()
